b58b088ebed7c88c7ad6d02ee8edebaa1ec48c3f
NaDiapla <nadiapla7@gmail.com>
2023-08-18 18:09:33 +0900

    chore: add auth related data to shared preference

--- presentation/src/main/java/com/seumulseumul/cld/sharedpref/PrefKey.kt
+++ presentation/src/main/java/com/seumulseumul/cld/sharedpref/PrefKey.kt
@@ -4,4 +4,16 @@ object PrefKey {
 
     // 로그인 여부: Boolean
     const val isLogin = "isLogin"
+
+    // OAuth Login Type(ex. kakao, google..): String
+    const val loginType = "loginType"
+
+    // OAuth access token
+    const val oAuthAccessToken = "oAuthAccessToken"
+
+    // JWT Token: String
+    const val authToken = "authToken"
+
+    // Refresh Token: String,
+    const val refreshToken = "refreshToken"
 }
