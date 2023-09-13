12df1dd770df9100f723ed0d2979259385cef0a9
nadiapla <nadiapla7@gmail.com>
2023-09-14 01:29:18 +0900

    chore: add location permission

--- presentation/src/main/java/com/seumulseumul/cld/ui/gym/GymDetailActivity.kt
+++ presentation/src/main/java/com/seumulseumul/cld/ui/gym/GymDetailActivity.kt
@@ -3,7 +3,7 @@ package com.seumulseumul.cld.ui.gym
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.seumulseumul.cld.databinding.ActivityGymDetailBinding
-import com.seumulseumul.cld.ui.term.TermDetailActivity
+import net.daum.mf.map.api.MapView
 
 class GymDetailActivity: AppCompatActivity() {
 
@@ -12,10 +12,16 @@ class GymDetailActivity: AppCompatActivity() {
     }
 
     private lateinit var binding: ActivityGymDetailBinding
+    private val mapView by lazy {
+        MapView(this@GymDetailActivity)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityGymDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+
+        binding.layoutMapView.addView(mapView)
     }
 }
--- presentation/src/main/java/com/seumulseumul/cld/ui/search/SearchNearGymsFragment.kt
+++ presentation/src/main/java/com/seumulseumul/cld/ui/search/SearchNearGymsFragment.kt
@@ -1,9 +1,18 @@
 package com.seumulseumul.cld.ui.search
 
+import android.Manifest
+import android. content.Context
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationManager
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AlertDialog
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Lifecycle
@@ -28,6 +37,11 @@ class SearchNearGymsFragment: Fragment() {
         GymsAdapter()
     }
 
+    private val accessLocationFlag = 1000
+
+    private var currentLat = 37.49609318770147
+    private var currentLon = 126.95428024925512
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -35,12 +49,14 @@ class SearchNearGymsFragment: Fragment() {
     ): View {
         _binding = FragmentSearchGymBinding.inflate(inflater, container, false)
 
+        permissionCheck()
+
         initView()
         initViewModelStream()
         viewModel.getClimbingGyms(
             PrefData.getString(PrefKey.authToken, ""),
-            126.95061461817554,
-            37.50299567151356,
+            currentLon,
+            currentLat,
             ""
         )
 
@@ -67,5 +83,59 @@ class SearchNearGymsFragment: Fragment() {
             it.layoutManager = LinearLayoutManager(requireContext())
             it.adapter = adapter
         }
+    }
+
+    // 위치 권한 확인
+    private fun permissionCheck() {
+        if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // 권한이 없는 상태
+            if (ActivityCompat.shouldShowRequestPermissionRationale(requireActivity(), Manifest.permission.ACCESS_FINE_LOCATION)) {
+                // 권한 거절 (다시 한 번 물어봄)
+                val builder = AlertDialog.Builder(requireContext())
+                builder.setMessage("위치 권한을 허용해주세요.")
+                builder.setPositiveButton("확인") { dialog, which ->
+                    ActivityCompat.requestPermissions(requireActivity(), arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), accessLocationFlag)
+                }
+                builder.setNegativeButton("취소") { dialog, which ->
+
+                }
+                builder.show()
+            } else {
+                ActivityCompat.requestPermissions(requireActivity(), arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), accessLocationFlag)
+            }
+        } else {
+            // 권한이 있는 상태
+            val locationManager = requireContext().getSystemService(Context.LOCATION_SERVICE) as LocationManager
+            val userLocation = if (locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER) != null) locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER)
+            else locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER)
+            if (userLocation != null) {
+                Log.d("TESTLOG", "userLocation.latitude: ${userLocation.latitude}")
+                Log.d("TESTLOG", "userLocation.longitude: ${userLocation.longitude}")
+                currentLat = userLocation.latitude
+                currentLon = userLocation.longitude
+            }
+        }
+    }
+
+    // 권한 요청 후 행동
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == accessLocationFlag) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // 권한 요청 후 승인됨 (추적 시작)
+                //Toast.makeText(this, "위치 권한이 승인되었습니다", Toast.LENGTH_SHORT).show()
+                //startTracking()
+            } else {
+                // 권한 요청 후 거절됨 (다시 요청 or 토스트)
+                //Toast.makeText(this, "위치 권한이 거절되었습니다", Toast.LENGTH_SHORT).show()
+                permissionCheck()
+            }
+        }
+    }
+
+    // GPS가 켜져있는지 확인
+    private fun checkLocationService(): Boolean {
+        val locationManager = requireContext().getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
     }
 }
